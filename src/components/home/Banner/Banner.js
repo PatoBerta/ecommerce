3818d2d8461ff73e3e569953e963a1ee7d2e2aae
Patricio Berteuris <berteurispatricio@gmail.com>
2023-08-31 13:16:39 -0300

    ecommerce agregado

---
+++ src/components/home/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>ReactCulture</h1>
+                <p>Vintage Clothes</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import Banner from "./Banner/Banner";
+import Navbar from "./Navbar/Navbar";
+const Home =() =>{
+    return (
+        <>
+            <Navbar />
+            <Banner />
+            <Products />
+        </>
+    );   
+};
+
+export default Home;
